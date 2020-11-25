98805997df48f6ef0457e90fddc57baaf51faa4e
cavalost <claudina.avalos@opendeusto.es>
2020-11-25 19:21:24 +0000

    feat: handle error server

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ const server = app.listen(PORT, () => {
 	const { port } = server.address();
 	console.log('Server', process.pid, 'listening on port', port);
 });
+
+server.on('error', error => {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	switch (error.code) {
+		case 'EACCES':
+			console.error(`Port ${PORT} requires elevated privileges`);
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(`Port ${PORT} is already in use`);
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+});
