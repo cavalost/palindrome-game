61ae24cedccb7f9adea0ea55a81ffbe1107dfaea
cavalost <claudina.avalos@opendeusto.es>
2020-11-25 22:42:03 +0000

    feat: add angular controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const counterRouter = require('./routes/wordRouter');
 const { handleErrors, handle404Error } = require('./utils/errorHandler');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
